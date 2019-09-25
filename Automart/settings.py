4bd2171da8b7eeadaa2c2e9bca70f1cc0cad29e1
michellearelygomezmeza@gmail.com <michelle.gomezmeza@here.com>
2019-09-25 18:40:10 -0500

    Creamos index y permisos de main app

--- Automart/settings.py
+++ Automart/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',                                  #En esta parte existe la jerarquia, por eso pusimos etsa al inicio
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
